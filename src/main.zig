90f1f91997d60760c908748bd94190ea2dd0df19
Kyle Coffey <me@phytolizer.dev>
2022-08-22 15:17:14 -0500

    initial code

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,92 @@
 const std = @import("std");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+const Allocator = std.mem.Allocator;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+const Todo = struct {
+    allocator: Allocator,
+    prefix: []u8,
+    suffix: []u8,
+    id: ?[]u8,
+    filename: []u8,
+    line: usize,
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    pub fn init(
+        allocator: Allocator,
+        prefix: []const u8,
+        suffix: []const u8,
+        id: ?[]const u8,
+        filename: []const u8,
+        line: usize,
+    ) !Todo {
+        return Todo{
+            .allocator = allocator,
+            .prefix = try allocator.dupe(u8, prefix),
+            .suffix = try allocator.dupe(u8, suffix),
+            .id = if (id) |i| try allocator.dupe(u8, i) else null,
+            .filename = try allocator.dupe(u8, filename),
+            .line = line,
+        };
+    }
 
-    try bw.flush(); // don't forget to flush!
+    pub fn deinit(self: *Todo) void {
+        self.allocator.free(self.prefix);
+        self.allocator.free(self.suffix);
+        if (self.id) |id| {
+            self.allocator.free(id);
+        }
+        self.allocator.free(self.filename);
+    }
+
+    pub fn format(self: *const Todo, fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+        _ = fmt;
+        _ = options;
+
+        try writer.print("{s}:{d}: {s}TODO", .{ self.filename, self.line, self.prefix });
+        if (self.id) |id| {
+            try writer.print("({s})", .{id});
+        }
+        try writer.print(": {s}", .{self.suffix});
+    }
+};
+
+fn todosOfDir(allocator: Allocator, dirpath: []const u8) ![]Todo {
+    _ = dirpath;
+    var todos = std.ArrayList(Todo).init(allocator);
+    try todos.append(try Todo.init(allocator, "// ", "khooy", "#42", "main.go", 10));
+    try todos.append(try Todo.init(allocator, "// ", "foo", null, "src/foo.go", 0));
+    return todos.toOwnedSlice();
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn listSubcommand(allocator: Allocator) !void {
+    var todos = try todosOfDir(allocator, ".");
+    defer allocator.free(todos);
+    for (todos) |*t| {
+        defer t.deinit();
+        try std.io.getStdOut().writer().print("{s}\n", .{t});
+    }
+}
+
+fn todo(comptime message: []const u8) noreturn {
+    std.debug.panic(message, .{});
+}
+
+fn reportSubcommand(allocator: Allocator) !void {
+    _ = allocator;
+    todo("report is not implemented");
+}
+
+pub fn main() !void {
+    var gpAllocator = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpAllocator.detectLeaks();
+    const allocator = gpAllocator.allocator();
+    var args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    if (std.mem.eql(u8, args[1], "list")) {
+        try listSubcommand(allocator);
+    } else if (std.mem.eql(u8, args[1], "report")) {
+        try reportSubcommand(allocator);
+    } else {
+        std.debug.panic("`{s}` unknown command", .{args[1]});
+    }
 }
